3631ede06ae41cb00b80c9b44eb904b45d96998a
Prihodko Nick <prihodkonn@gmail.com>
2020-05-05 17:36:59 +0300

    Updates

---
+++ precache-manifest.211b59d6b314700ef1c548a8158aec08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "217da1819fc627644e1e4d80faa3140d",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "a7d7f90459060cef4387",
+    "url": "/react-todo/static/css/main.5cefea1c.chunk.css"
+  },
+  {
+    "revision": "8c8be03abacfdda0a87a",
+    "url": "/react-todo/static/js/2.f899221b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.f899221b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d7f90459060cef4387",
+    "url": "/react-todo/static/js/main.a427665f.chunk.js"
+  },
+  {
+    "revision": "2294b07f0cda16a9954b",
+    "url": "/react-todo/static/js/runtime-main.dd2745f3.js"
+  }
+]);
